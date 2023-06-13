b5d268f802581a4109cc71e8a3f532b05de7d2cd
So8oS <Ahmadothman033@gmail.com>
2023-06-13 15:18:25 +0400

    initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-img-element */
+import "./../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="">
+      <div className="flex h-12 justify-between items-center bg-[#eee5e5] p-2 ">
+        <div className="flex justify-center items-center gap-1">
+          <img className="w-10" src="/logo.png" alt="" />
+          <h1 className="text-2xl font-Pacifico font-bold">Le Khalil Le Cookies</h1>
+        </div>
+
+        <ul className="flex justify-center items-center gap-1 text-lg">
+          <li>Who Are We</li>
+          <li></li>
+          <img className="w-6" src="./bag.svg" alt="" />
+        </ul>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
