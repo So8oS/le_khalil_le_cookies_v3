c4a89cd806b8a2fed2acef66aee11269d191d492
So8oS <Ahmadothman033@gmail.com>
2023-07-01 14:35:57 +0400

    all emails work

---
+++ src/pages/api/orderStatus.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+import type { NextApiRequest, NextApiResponse } from "next";
+import Welcome from "../../emails/welcome";
+import { Resend } from "resend";
+import Status from "../../emails/status";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("yo");
+
+  const { email, status } = req.body;
+  console.log(email, status);
+
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Order Status",
+      //@ts-ignore
+      react: Status({ email, status }),
+    });
+
+    res.status(200).json(` sent to ${email}`);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
