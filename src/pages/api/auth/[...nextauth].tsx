9ee9538362f9b6437133c0f61a494556ab4cdc0c
So8oS <Ahmadothman033@gmail.com>
2023-07-06 20:24:59 +0400

    try

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -6,44 +6,43 @@ import prismadb from "../../../../lib/prismadb";
 
 export const authOptions = {
   providers: [
-    process.env.VERCEL_ENV === "preview" &&
-      Credentials({
-        id: "credentials",
-        name: "Credentials",
-        credentials: {
-          email: {
-            label: "Email",
-            type: "text",
-          },
-          password: {
-            label: "Password",
-            type: "password",
-          },
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
         },
-        async authorize(credentials) {
-          if (!credentials?.email || !credentials?.password) {
-            throw new Error("Email and password required");
-          }
-
-          const user = await prismadb.user.findUnique({
-            where: {
-              email: credentials.email,
-            },
-          });
-
-          if (!user || !user.hashedPassword) {
-            throw new Error("Email does not exist");
-          }
-
-          const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
-
-          if (!isCorrectPassword) {
-            throw new Error("Incorrect password");
-          }
-
-          return user;
+        password: {
+          label: "Password",
+          type: "password",
         },
-      }),
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
   ],
 
   pages: {
